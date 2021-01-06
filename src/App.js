08184e1a9b3f72b599ab093132054d84bfbc0440
Lotuslinny <linny@fegg.nl>
2021-01-06 16:21:26 +0100

    Changing State Part 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, { useState } from "react"
 
 function App() {
   const [count, setCount] = useState(0)
-  //const [answer, setAnswer] = useState("Yes")
+  //const [answer, setAnswer] = useState("Yes").
 
   function increment() {
     setCount(prevCount => prevCount + 1)
